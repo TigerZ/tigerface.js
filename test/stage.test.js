b52e65436ed07df5409c570668aa70cc9673b817
TigerZ <zhangyihu@gmail.com>
2018-03-05 22:53:02 +0800

    重构显示包

--- test/stage.test.js
+++ test/stage.test.js
@@ -1,7 +1,8 @@
 import assert from 'assert';
 import {Event} from 'tigerface-event';
 import Stage from '../src/tigerface-display/Stage';
-import { JSDOM } from "jsdom";
+import {JSDOM} from "jsdom";
+
 const {document} = new JSDOM(`<!DOCTYPE html><div></div>`).window;
 const dom = document.querySelector('div');
 
