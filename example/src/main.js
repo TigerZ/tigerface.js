626a8bfff7d799f250b69f98619d6640c148685b
TigerZ <zhangyihu@gmail.com>
2018-03-14 01:07:34 +0800

    整理系统事件接口

--- example/src/main.js
+++ example/src/main.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {AppContainer} from 'react-hot-loader'
-import AppRoot from './AppRoot';
+import AppRoot from './ChartsAppRoot';
 import $ from 'jquery';
 window.$ = $;
 
