74dcb900337079cbf95ca84a35fff4fddab82a6a
TigerZ <zhangyihu@gmail.com>
2018-08-01 23:46:15 +0800

    ppt 范例编写中，配合重构架构细节

--- packages/tigerface-common/publish.sh
+++ packages/tigerface-common/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm -rf ./lib
-babel -q --no-babelrc --presets=env,stage-1,react --optional es7.objectRestSpread ./src --out-dir ./lib
+babel -q --optional es7.objectRestSpread ./src --out-dir ./lib
 mv index.js dev.js
 mv npm.js index.js
 npm publish
